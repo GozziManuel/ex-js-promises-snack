d2725493abf3e33afaa76bb3097172317a6fc8eb
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-04-27 23:36:09 +0200

    bonus 2nd

--- Promises.js
+++ Promises.js
@@ -1,15 +1,24 @@
+let rememberNumber = null;
 function lanciaDado() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const value = Math.round(Math.random() * (6 - 1) + 1);
+      const value = Math.round(Math.random() * (2 - 1) + 1);
+
+      if (value === rememberNumber) {
+        resolve("WOW STESSO RISULTATO");
+      }
       if (value > 1) {
         resolve(value);
       } else {
-        reject("inceppato");
+        reject(`inceppato ${value}`);
       }
+      rememberNumber = value;
     }, 3000);
   });
 }
+lanciaDado()
+  .then((num) => console.log(num))
+  .catch((err) => console.error(err));
 lanciaDado()
   .then((num) => console.log(num))
   .catch((err) => console.error(err));
